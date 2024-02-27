86f76e126b6ffaa3037d5768efbfe7940285244a
Flesh <olivia.a.gowan@gmail.com>
2024-02-27 14:41:08 -0500

    added BlogPost and Comment models + all associations

--- models/index.js
+++ models/index.js
@@ -1,5 +1,35 @@
 const User = require('./User');
+const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
 
+// User has many blogposts
+User.hasMany(BlogPost, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User };
+// User has many comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// Blogpost has many comments
+BlogPost.hasMany(Comment, {
+  foreignKey: 'blogpost_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(BlogPost, {
+  foreignKey: 'blogpost_id'
+});
+
+module.exports = { User, BlogPost, Comment};
