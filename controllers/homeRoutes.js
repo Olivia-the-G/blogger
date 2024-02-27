73cb0048d444bcc21a4c9c67072ce1bb755a98df
Flesh <olivia.a.gowan@gmail.com>
2024-02-27 18:24:24 -0500

    adding homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,21 @@
+const { BlogPost } = require('../models');
+const { User } = require('../models');
+const { Comment } = require('../models');
+
 const router = require('express').Router();
+
+
+  router.get('/', async (req, res) => {
+    try {
+      res.render('homepage', { 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err);
+    }
+  });
+
 
 
 module.exports = router;
