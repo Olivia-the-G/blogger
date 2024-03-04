652b597bff7bb32dc9e1f44ce46fb68491c2bad8
Flesh <olivia.a.gowan@gmail.com>
2024-03-03 23:37:49 -0500

    added logout button and functionality

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,23 @@
-// when user is signed in the sign in button turns into a sign out button
-
-// logout option logs user out of the site
 
 // when idle for a certain amount of time the user can still view posts but needs to sign in to make a post, update or delete
 
+const logout = async () => {
+  try {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+
+// Call the logout function when the logout button is clicked
+document.getElementById('logoutBtn').addEventListener('click', logout);
